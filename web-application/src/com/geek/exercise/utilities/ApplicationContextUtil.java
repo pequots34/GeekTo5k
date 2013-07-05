0a9ed7c2bc1af622ac30d3710245c3ed5e224e7e
Pequots34 <christopherbrown34@gmail.com>
2013-07-05 15:28:35 -0400

    stub web app

---
+++ web-application/src/com/geek/exercise/utilities/ApplicationContextUtil.java
@@ -0,0 +1,28 @@
+package com.geek.exercise.utilities;
+
+import org.springframework.beans.BeansException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+
+public class ApplicationContextUtil implements ApplicationContextAware {
+
+	private static ApplicationContext mContext;
+
+	public ApplicationContextUtil() {
+		super();
+	}
+
+	@Override
+	public void setApplicationContext( ApplicationContext context ) throws BeansException {
+		mContext = context;
+	}
+	
+	public static Object getBeanByName( String name ) {
+		if ( mContext != null && mContext.containsBean( name ) ) {
+			return mContext.getBean( name );
+		}
+		
+		return null;
+	}
+
+}
---
+++ web-application/src/com/geek/exercise/utilities/PropertyPlaceholderUtil.java
@@ -0,0 +1,43 @@
+package com.geek.exercise.utilities;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Properties;
+
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
+import org.springframework.beans.factory.config.PropertyPlaceholderConfigurer;
+import org.springframework.util.PropertyPlaceholderHelper;
+
+public class PropertyPlaceholderUtil extends PropertyPlaceholderConfigurer {
+
+	private static final String PROPERTY_PREFIX = "${";
+	
+	private static final String PROPERTY_SUFFIX = "}";
+	
+	private final PropertyPlaceholderHelper mPlaceholder = new PropertyPlaceholderHelper( PROPERTY_PREFIX, PROPERTY_SUFFIX );
+
+	private static Map<String, String> mProperties;
+	
+	private PropertyPlaceholderUtil() {
+		super();
+	}
+
+	@Override
+	protected void processProperties( ConfigurableListableBeanFactory beanFactoryToProcess, Properties props ) throws BeansException {
+		super.processProperties(beanFactoryToProcess, props);
+		
+		mProperties = new HashMap<String, String>();
+		
+		for( Object entry : props.keySet() ) {
+			String key = entry.toString();
+			
+			String value = mPlaceholder.replacePlaceholders( props.getProperty( key.toString() ), props);
+			
+			if ( key != null && !key.isEmpty() && value != null && !value.isEmpty() ) {
+				mProperties.put(key, value);
+			}
+		}
+	}
+	
+}
